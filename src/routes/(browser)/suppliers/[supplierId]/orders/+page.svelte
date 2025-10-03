4ac2afaaa23cd94b10c2d7418402c4f7c36f577b
Thomas Mahringer <thomas@plancise.com>
2025-10-03 11:56:28 +0200

    add route /suppliers/X/orders

--- src/routes/(browser)/suppliers/[supplierId]/orders/+page.svelte
+++ src/routes/(browser)/suppliers/[supplierId]/orders/+page.svelte
@@ -1,7 +1,9 @@
+<!-- src/routes/suppliers/[supplierId]/+page.svelte -->
 <script lang="ts">
   /**
    * Route Delegator (UI)
    */
+  // SupplierDetailPage handles children: categories, orders.
   import SupplierDetailPage from '$lib/components/domain/suppliers/SupplierDetailPage.svelte';
   let { data } = $props();
 </script>
---
+++ src/routes/(browser)/suppliers/[supplierId]/orders/+page.ts
@@ -0,0 +1,7 @@
+// src/routes/suppliers/[supplierId]/orders/+page.ts
+
+/**
+ * Route Delegator (Logic)
+ */
+// supplierDetailPage handles children: categories, orders.
+export { load } from '$lib/components/domain/suppliers/supplierDetailPage';

de2eb6ab09e45481448b35999480e8e4b1cb94d2
Thomas Mahringer <thomas@mindruptive.com>
2025-11-29 16:37:00 +0100

    Add @sveltejs/acorn-typescript to devDependencies and update workspace configuration
    - Added @sveltejs/acorn-typescript version 1.0.7 to package.json for TypeScript support.
    - Updated pure-svelte.code-workspace to include a new path for improved project structure.

---
+++ pure-open-cmds.cmd
@@ -0,0 +1,27 @@
+@echo off
+wt ^
+new-tab -p "Dev" --title "svelte check" cmd /k "cd /d \dev\pure\pure-svelte && npm run check:watch" ; ^
+new-tab -p "Dev" --title "dev" cmd /k "cd /d \dev\pure\pure-svelte && npm run dev" ; ^
+new-tab -p "Dev" --title "tools watch" cmd /k "cd /d \dev\pure\pure-svelte && npm run tools:watch" ; ^
+new-tab -p "Dev" --title "shopify watch" cmd /k "cd /d \dev\pure\pure-shopify && npm run watch" ; ^
+new-tab -p "Dev" --title "pure-svelte" cmd /k "cd /d \dev\pure\pure-svelte"
+
+
+echo: NOTE: Add following to win termin profiles.json:
+(
+  echo             {
+  echo                 "name": "Dev",
+  echo                 "guid": "{2c8f4918-62d3-41a6-9a25-8370216694e1}",
+  echo                 "commandline": "%%SystemRoot%%\\System32\\cmd.exe",
+  echo                 "suppressApplicationTitle": true,
+  echo                 "hidden": false,
+  echo                 "font": 
+  echo                 {
+  echo                     "size": 11
+  echo                 },
+  echo                 "colorScheme": "Tango Light",
+  echo                 "historySize": 12000
+  echo             }
+)
+
+pause
